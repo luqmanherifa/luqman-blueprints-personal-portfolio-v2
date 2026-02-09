74883b9bcba9d59b37044943b2f3ae5c0efd49b3
Luqman Herifa <luqmanherifa@gmail.com>
2026-02-09 13:12:47 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,22 +15,19 @@ module.exports = {
         "bounce-fast": "bounce 0.3s infinite",
         "pulse-fast": "pulse 0.8s infinite",
       },
+      transitionProperty: {
+        theme: "background-color, border-color, color, fill, stroke",
+      },
+      transitionDuration: {
+        theme: "300ms",
+      },
     },
     screens: {
       "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-
       xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
       lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-
       md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
-
       sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
     },
   },
   plugins: [require("@tailwindcss/line-clamp")],
